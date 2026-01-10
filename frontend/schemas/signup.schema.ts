2f6fcb3f30f63b54422121f1379bd4f959c796e6
nikolinaasarac <nikolinasarac28@gmail.com>
2026-01-10 19:50:26 +0100

    Add signup schema and update email validation message in login schema

---
+++ frontend/schemas/signup.schema.ts
@@ -0,0 +1,8 @@
+import * as yup from 'yup';
+export const signupSchema = yup.object({
+	name: yup.string().required("Ime je obavezno"),
+	surname: yup.string().required("Prezime je obavezno"),
+	email: yup.string().email("Invalid email").required("Email je obavezan"),
+	password: yup.string().required("Lozinka je obavezna").min(8, "Lozinka mora imati najmanje 8 karaktera"),
+	confirmPassword: yup.string().required("Potvrda lozinke je obavezna").oneOf([yup.ref('password')], 'Lozinke se ne podudaraju')
+}).required();
