908d08e90f5f75bcf9896aff118d0b58d1b8261d
nikolinaasarac <nikolinasarac28@gmail.com>
2026-02-28 16:21:18 +0100

    Refactor `VenueForm` to use `useApp` context for cities and venue types

--- frontend/context/app-context.tsx
+++ frontend/context/app-context.tsx
@@ -2,9 +2,15 @@
 import {createContext, useContext, useEffect, useMemo, useState} from "react";
 import {EventCategory} from "@/models/event-category.model";
 import EventCategoryService from "@/services/event-category.service";
+import {City} from "@/models/city.model";
+import CitiesService from "@/services/cities.service";
+import {VenueType} from "@/models/venue-type.model";
+import VenueTypesService from "@/services/venue-types.service";
 
 interface AppContextProps {
 	eventCategories: EventCategory[];
+	cities: City[];
+	venueTypes: VenueType[];
 	loading: boolean;
 }
 
@@ -12,14 +18,18 @@ const AppContext = createContext<AppContextProps | undefined>(undefined);
 
 export function AppProvider({children}: { children: React.ReactNode }) {
 	const [eventCategories, setEventCategories] = useState<EventCategory[]>([]);
+	const [cities, setCities] = useState<City[]>([]);
+	const [venueTypes, setVenueTypes] = useState<VenueType[]>([]);
 	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		const fetchData = async () => {
 			setLoading(true);
 			try {
-				const response = await EventCategoryService.getEventCategories();
-				setEventCategories(response);
+				const [citiesRes, categoriesRes, venueTypesRes] = await Promise.all([CitiesService.getCities(), EventCategoryService.getEventCategories(), VenueTypesService.getVenueTypes()]);
+				setEventCategories(categoriesRes);
+				setCities(citiesRes);
+				setVenueTypes(venueTypesRes);
 			} catch (error) {
 				console.error(error);
 			} finally {
@@ -29,8 +39,8 @@ export function AppProvider({children}: { children: React.ReactNode }) {
 		fetchData();
 	}, [])
 
-	const value = useMemo(() => ({loading, eventCategories}),
-		[loading, eventCategories]
+	const value = useMemo(() => ({loading, eventCategories, cities, venueTypes}),
+		[loading, eventCategories, cities, venueTypes]
 	);
 
 	return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
