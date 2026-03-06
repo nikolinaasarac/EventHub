caf835c278e6f1e594066aedc776e99c74cd5e16
nikolinaasarac <nikolinasarac28@gmail.com>
2026-03-06 08:53:28 +0100

    Add `EditLocationLayout` with authentication and role-based access control
    - Introduced `layout.tsx` for `edit-location` page to enforce authentication and restrict access to admins.
    - Added success toast for ticket purchase confirmation in event view.

---
+++ frontend/app/(dashboard)/locations/edit-location/[id]/layout.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import {useAuth} from "@/context/auth-context";
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
+import {UserRole} from "@/shared/enums/user-role.enum";
+import {Spinner} from "@/components/ui/spinner";
+
+export default function Layout({children}: { children: React.ReactNode }) {
+	const {user, isLoading} = useAuth();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (!isLoading) {
+			if (!user) {
+				router.push("/login");
+			} else if (!user.roles.some(r => r.name === UserRole.ADMIN)) {
+				router.push("/home");
+			}
+		}
+	}, [user, isLoading, router]);
+
+	if (isLoading || !user) {
+		return <div className="h-screen flex justify-center items-center mb-6">
+			<Spinner className="w-15 h-15"/>
+		</div>
+	}
+
+	return <>{children}</>;
+}
